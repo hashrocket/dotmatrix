a1e1370dc36ed6212c5ea9c46af4a5662dc17eaf
Rogelio J. Samour <rogelio@therubymug.com>
2010-04-22 17:50:48 -0400

    Add vimbundles installation script

---
+++ bin/vimbundles.sh
@@ -0,0 +1,36 @@
+
+mkdir -p ~/.vimbundles
+cd ~/.vimbundles
+
+get_bundle() {
+	(
+	if [ -d "$2" ]; then
+		echo "Updating $1's $2"
+		cd "$2"
+		git pull --rebase
+	else
+		git clone "git://github.com/$1/$2.git"
+	fi
+	)
+}
+
+get_bundle pangloss vim-javascript
+get_bundle scrooloose nerdcommenter
+get_bundle therubymug vim-pyte
+get_bundle tpope vim-abolish
+get_bundle tpope vim-cucumber
+get_bundle tpope vim-endwise
+get_bundle tpope vim-flatfoot
+get_bundle tpope vim-fugitive
+get_bundle tpope vim-git
+get_bundle tpope vim-haml
+get_bundle tpope vim-markdown
+get_bundle tpope vim-pathogen
+get_bundle tpope vim-ragtag
+get_bundle tpope vim-rails
+get_bundle tpope vim-repeat
+get_bundle tpope vim-speeddating
+get_bundle tpope vim-surround
+get_bundle tpope vim-unimpaired
+get_bundle tpope vim-vividchalk
+get_bundle wgibbs vim-irblack
