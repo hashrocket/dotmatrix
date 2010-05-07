a36f758e09d497f64c041a9823c63339b17792f4
Les Hill <leshill@gmail.com>
2010-05-07 10:27:05 -0400

    Cleanup

--- bin/vimbundles.sh
+++ bin/vimbundles.sh
@@ -1,17 +1,16 @@
-
 mkdir -p ~/.vimbundles
 cd ~/.vimbundles
 
 get_bundle() {
-	(
-	if [ -d "$2" ]; then
-		echo "Updating $1's $2"
-		cd "$2"
-		git pull --rebase
-	else
-		git clone "git://github.com/$1/$2.git"
-	fi
-	)
+  (
+  if [ -d "$2" ]; then
+    echo "Updating $1's $2"
+    cd "$2"
+    git pull --rebase
+  else
+    git clone "git://github.com/$1/$2.git"
+  fi
+  )
 }
 
 get_bundle leshill vim-json
