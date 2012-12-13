57c7556490cb04f06580f90e597f8634e1cefb0b
Joshua Davey and Jon Allured <dev+jgdavey+jonallured@hashrocket.com>
2012-12-13 17:06:17 -0600

    Include .hashrc again

--- bin/file_list.sh
+++ bin/file_list.sh
@@ -5,7 +5,7 @@ dir=$PWD
 
 # Get list of files to link
 includes=".vim .zsh"
-excludes=".hashrc"
+excludes=".gitignore"
 base="$(find . -depth 1 -name '.*' -not -name '.*.local' -type f | sed 's#^\./##' | grep -vF $excludes)"
 files="$base $includes"
 echo $files
