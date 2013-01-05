6a359b7d4a3f93ec518ab2c3012ce0425513f9d7
Joshua Davey <josh@joshuadavey.com>
2013-01-05 13:47:44 -0600

    Use the newer zsh completion engine

--- completions/hr.zsh
+++ completions/hr.zsh
@@ -1,19 +1,25 @@
-if [[ ! -o interactive ]]; then
-    return
-fi
+_hr_commands() {
+  local cmds
+  local -a commands
+  cmds="$(hr commands)"
+  commands=(${(ps:\n:)cmds})
+  _wanted command expl "hr command" compadd -a commands
+}
 
-compctl -K _hr hr
+_hr_subcommands() {
+  local cmd subcmds
+  local -a commands
+  cmd="${words[2]}"
+  subcmds="$(hr completions ${words[2,$(($CURRENT - 1))]})"
+  commands=(${(ps:\n:)subcmds})
+  _wanted subcommand expl "hr $cmd subcommand" compadd -a commands
+}
 
 _hr() {
-  local word words completions
-  read -cA words
-  word="${words[2]}"
-
-  if [ "${#words}" -eq 2 ]; then
-    completions="$(hr commands)"
-  else
-    completions="$(hr completions "${word}" ${words[@]:2})"
-  fi
-
-  reply=("${(ps:\n:)completions}")
+  case $CURRENT in
+    2) _hr_commands ;;
+    *) _hr_subcommands ;;
+  esac
 }
+
+compdef _hr hr
