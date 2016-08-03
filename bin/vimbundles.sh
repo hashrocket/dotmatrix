f1f45c27799a475a31f0f3e1ba497061d71c5f91
Joshua Davey <josh@joshuadavey.com>
2016-08-03 09:58:26 -0400

    Use `hr vimbundle` directly in vimbundles.sh
    Addresses #46

--- bin/vimbundles.sh
+++ bin/vimbundles.sh
@@ -6,55 +6,7 @@ warn() {
 
 
 warn "$0 is deprecated and will be removed."
-warn "Please use hr's vimbundle command (it's faster and better)."
+warn "Please run \`hr vimbundle\` directly."
 echo
 
-if [ ! -d "$HOME/.vimbundles" ]; then
-  BASE="$HOME/.vim/bundle"
-else
-  BASE="$HOME/.vimbundles"
-fi
-
-if [ "$1" = "log-since" ]; then
-  echo "**logsince**"
-  logsince="true"
-  date="$2"
-else
-  logsince="false"
-fi
-
-if $($logsince) ; then
-  echo "*** logsince ***"
-fi
-
-mkdir -p $BASE
-
-action_from() {
-  if [ -r $1 ]; then
-    repos="$(cat $1)"
-    for repo in $repos; do
-      cd $BASE
-      dir="$(basename $repo)"
-      if [ -d "$BASE/$dir" ]; then
-        cd "$BASE/$dir"
-        if $logsince ; then
-          echo "Log for repo $repo" >>/tmp/vimbundles.log
-          git log --since $date --color=always >>/tmp/vimbundles.log
-        else
-          echo "Updating $repo"
-          git pull --depth=1 --rebase
-        fi
-      else
-        git clone --depth=1 https://github.com/"$repo".git
-      fi
-    done
-  fi
-}
-
-action_from "$HOME/.vimbundle"
-action_from "$HOME/.vimbundle.local"
-
-if $logsince ; then
-  less "/tmp/vimbundles.log"
-  rm "/tmp/vimbundles.log"
-fi
+$PWD/hr/bin/hr vimbundle
